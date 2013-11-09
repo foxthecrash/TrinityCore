b76d758a550cd658d40ff6f14f5227ee6e8eb4b1
Bengelbaer <bengelbaer@gmx.de>
2013-11-09 13:33:03 +0100

    MOD:  Classes/Races can earn extratalents Rewards from Quest.
    Signed-off-by: Bengelbaer <bengelbaer@gmx.de>

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -24668,17 +24668,20 @@ void Player::StoreLootItem(uint8 lootSlot, Loot* loot)
 
 uint32 Player::CalculateTalentsPoints() const
 {
-    uint32 base_talent = getLevel() < 10 ? 0 : getLevel()-9;
+    uint32 base_level = getClass() == CLASS_DEATH_KNIGHT ? 55 : 9;
+	uint32 base_talent = getLevel() <= base_level ? 0 : getLevel() - base_level;
+	
+	uint32 talentPointsForLevel = base_talent + m_questRewardTalentCount;
 
-    if (getClass() != CLASS_DEATH_KNIGHT || GetMapId() != 609)
+/*    if (getClass() != CLASS_DEATH_KNIGHT || GetMapId() != 609)
         return uint32(base_talent * sWorld->getRate(RATE_TALENT));
 
-    uint32 talentPointsForLevel = getLevel() < 56 ? 0 : getLevel() - 55;
+    uint32 talentPointsForLevel = getLevel() < 56 ? 0 : getLevel() - 55;	// OrginalCode TalentPoints Adding 
     talentPointsForLevel += m_questRewardTalentCount;
 
     if (talentPointsForLevel > base_talent)
         talentPointsForLevel = base_talent;
-
+*/
     return uint32(talentPointsForLevel * sWorld->getRate(RATE_TALENT));
 }
 
